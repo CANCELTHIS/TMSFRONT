b0fcbc982a37781328c3ba1560b1af9457179b1e
rebuma <rebumatamiru12@gmail.com>
2025-01-14 12:11:33 +0300

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -7,44 +7,46 @@ import Hero2 from './components/Hero2';
 import Hero3 from './components/Hero3';
 import EmailForm from './components/EmailForm';
 import Footer from './components/Footer';
-<<<<<<< HEAD
-import Login from './components/Login';
+import Login from './components/Login';  // Import Login page
 import Signup from './components/Signup';
-
-const App = () => {
-  return (
-    <Router>
-      <div className="app" id="home">
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Hero1 />} />
-          <Route path="/services" element={<Hero2 />} />
-          <Route path="/about" element={<Hero3 />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/Signup" element={<Signup />} />
-          <Route path="/contact" element={<EmailForm />} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
-=======
 import { LanguageProvider } from './context/LanguageContext'; // Updated relative path
 
 const App = () => {
   return (
-    <LanguageProvider>
-      <div className="app" id="home">
-        <NavBar />
-        <Hero1 />
-        <Hero2 />
-        <Hero3 />
-        <Hero4 />
-        <Hero5 />
-        <EmailForm />
-        <Footer />
-      </div>
-    </LanguageProvider>
->>>>>>> 71aee9073c18f7bc6891e7f79bafd9264f87e7f2
+    <Router>
+      <LanguageProvider>
+        <div className="app" id="home">
+          <NavBar />
+          
+          {/* Home Section */}
+          <div id="home">
+            <Hero1 />
+          </div>
+          
+          {/* Services Section */}
+          <div id="services">
+            <Hero2 />
+          </div>
+          
+          {/* About Section */}
+          <div id="about">
+            <Hero3 />
+          </div>
+
+          {/* Contact Section */}
+          <div id="contact">
+            <EmailForm />
+          </div>
+          
+          <Routes>
+            <Route path="/login" element={<Login />} />  {/* Add Login route */}
+            <Route path="/Signup" element={<Signup />} />
+          </Routes>
+          
+          <Footer />
+        </div>
+      </LanguageProvider>
+    </Router>
   );
 };
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,103 +1,70 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import Logo from "../assets/Logo.jpg";
 import "../index.css";
 import { IoMdArrowDropdown } from "react-icons/io";
 import { useLanguage } from '../context/LanguageContext'; // Importing from context
 import { FaLanguage } from "react-icons/fa6";
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+
 const NavBar = () => {
-  // Access the global language state and toggle function from the context
-  const { mylanguage, toggleLanguage } = useLanguage();
+  const { mylanguage, toggleLanguage } = useLanguage(); // Correct usage
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-light">
-      <div className="container-fluid">
-        <Link className="navbar-brand" to="/">
-          <img id="logo" src={Logo} alt="MiNT Logo" />
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav ms-auto mb-2 mb-lg-0 d-flex justify-content-center">
-            <section className="navs">
-              <li className="nav-item">
-                <Link className="nav-link" to="/">Home</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/services">Services</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/about">About</Link>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">EN <IoMdArrowDropdown /></a>
-              </li>
-            </section>
-          </ul>
-          <Link
-            className="btn"
-            to="/login"
-            style={{
-              backgroundColor: '#F09F33',
-              color: 'white',
-              marginTop: '-5px'
-            }}
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#home">
+            <img id="logo" src={Logo} alt="MiNT Logo" />
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
           >
-<<<<<<< HEAD
-            Login
-          </Link>
-=======
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0 d-flex justify-content-center">
               <section className="navs">
                 <li className="nav-item">
-                  <a className="nav-link" aria-current="page" href="#">
-                    {mylanguage === 'EN' ? 'Home' : 'መግቢያ'}
-                  </a>
+                  <a className="nav-link" href="#home">{mylanguage === 'EN' ? 'Home' : 'መግቢያ'}</a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    {mylanguage === 'EN' ? 'Services' : 'አገልግሎቶች'}
-                  </a>
+                  <a className="nav-link" href="#services">{mylanguage === 'EN' ? 'Services' : 'አገልግሎቶች'}</a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    {mylanguage === 'EN' ? 'About' : 'ስለ እኛ'}
-                  </a>
+                  <a className="nav-link" href="#about">{mylanguage === 'EN' ? 'About' : 'ስለ እኛ'}</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#contact">{mylanguage === 'EN' ? 'Contact Us' : 'እኛን እንደምን ደርሶ'}</a>
                 </li>
                 <li className="nav-item">
                   <a className="nav-link" href="#" onClick={toggleLanguage}>
-                  {mylanguage === 'EN' ? 'EN' : 'አማ'} <FaLanguage size={30}/>
+                    {mylanguage === 'EN' ? 'EN' : 'አማ'} <FaLanguage size={30} />
                   </a>
                 </li>
               </section>
             </ul>
-            <button
-              className="btn"
-              style={{
-                backgroundColor: '#F09F33',
-                color: 'white',
-                marginTop: '-5px'
-              }}
-            >
-              {mylanguage === 'EN' ? 'Login' : 'መግቢያ'}
-            </button>
+            <a href="/Login">
+              <button
+                className="btn"
+                style={{
+                  backgroundColor: '#F09F33',
+                  color: 'white',
+                  marginTop: '-5px'
+                }}
+              >
+                {mylanguage === 'EN' ? 'Login' : 'መግቢያ'}
+              </button>
+              </a>
           </div>
->>>>>>> 71aee9073c18f7bc6891e7f79bafd9264f87e7f2
         </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
   );
 };
 
